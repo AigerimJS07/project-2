ea1908874a3de2d595de2339dd94ee7dd64897b6
AigerimJS07 <aigeimfront@gmail.com>
2025-04-16 20:57:27 +0600

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,16 @@ function App() {
     setNamber((prev)  => prev -10 ) ;
   }
 
+  function reset  () {
+    setNamber(0);
+  }
 
   return(
     <div>
     <h1>{number}</h1>
     <button onClick={increment}>+10</button>
     <button onClick={decrement}>-10</button>
+    <button onClick={reset}>reset</button>
     </div>
   );
   
